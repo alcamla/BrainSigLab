95692003128d0f5e6d297d83bfb8523b60921e5c
Alejandro Camacho Laverde <camacholaverde@gmail.com>
2015-03-27 18:07:53 -0500

    Initial Commit

--- BrainSigLab/ViewController.swift
+++ BrainSigLab/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signalsImageButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        signalsImageButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("didTapSignalsImageButton:")))
+
+    }
+    
+    func didTapSignalsImageButton(tap:UITapGestureRecognizer){
+        
+        
+        //Present the signalsViewController
+        let signalsViewController = self.storyboard!.instantiateViewControllerWithIdentifier("signalsViewController") as! GBCSignalsViewController
+        
+        presentViewController(signalsViewController, animated: true, completion: nil)
+        
+        
+    }
 
 
 }
 
+
+extension ViewController: UIViewControllerTransitioningDelegate{
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return nil
+    }
+    
+}
